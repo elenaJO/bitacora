55b35b9c3bd927796e0dae0c2aa8a4ff29e7733a
elenaJO <ele.lucero1993@gmail.com>
2018-02-19 19:46:10 -0500

    corrigiendo errores

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
   var containerContext = $('#container-content');
+  var image;
+  var opcion;
+  var video;
   $('.modal').modal();
 
   $('.datepicker').pickadate({
@@ -10,15 +13,10 @@ $(document).ready(function () {
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
   });
-        
-  
-  // agregar mensaje
-  $('#message').click(function(){
-    $('#title-message').val('');
-    $('#text-message').val('');
-  });
 
-  $('#public-message').click(function(){
+
+  // agregar mensaje
+  $('#public-message').click(function () {
     var titleMessage = $('#title-message').val();
     var textMessage = $('#text-message').val();
     var appen = `<div class="row">
@@ -27,88 +25,94 @@ $(document).ready(function () {
                      <p>${textMessage}</p>
                    </div>
                 </div>`
-   containerContext.append(appen);
+    containerContext.append(appen);
+    $('#title-message').val('');
+    $('#text-message').val('');
   });
 
   // agregar imagen
-  $('#fileButton').change(function() {
-    var titleImage = $('#title-image').val();
+  $('#fileButton').change(function () {
     var reader = new FileReader();
-    reader.onload = function(event) {
-      var appen = `<div class="row">
-                   <div class="col s12 m8 push-m2 publications">
-                   <h3>${titleImage}</h3>
-                   <img src="_pub_" alt="" class="img-pub">
-                   </div>
-                </div>`
-      var appenReplace = appen.replace('_pub_', event.target.result);
-      containerContext.append(appenReplace);
+    reader.onload = function (event) {
+      image = event.target.result;
     };
     reader.readAsDataURL(this.files[0]);
   });
 
+  $('#public-image').click(function () {
+    var titleImage = $('#title-image').val();
+    var appen = `<div class="row">
+                   <div class="col s12 m8 push-m2 publications">
+                   <h3>${titleImage}</h3>
+                   <img src=${image} alt="" class="img-pub">
+                   </div>
+                </div>`
+    containerContext.append(appen);
+    $('.file-path').val('');
+    $('#title-image').val('');
+  });
 
   // agregar evento
 
-  $('#public-date').click(function(){
+  $('#public-date').click(function () {
     var titleDate = $('#title-date').val();
     var date = $('#date').val();
-    function localizacion(posicion){
-      var latitude = posicion.coords.latitude;
-      var longitude = posicion.coords.longitude;
-      // console.log(longitude);
-      var appen = `<div class="row">
-                   <div class="col s12 m8 push-m2 publications">
-                   <h3>${titleDate}</h3>
-                   <p>${date}</p>
-                   <img src="_pub_" alt="" class="img-pub">
-                   </div>
-                </div>`
-     var urlImage = 'https://maps.googleapis.com/maps/api/staticmap?center='+ latitude + ',' + longitude + '&zoom=16&size=2000x1200&markers=color:red%7C'+latitude+','+longitude+'&key=AIzaSyDnxULnKl1u7sIJVCbUgVlm44D69GE2AFA'
-     var appenReplace = appen.replace('_pub_', urlImage);
-     containerContext.append(appenReplace); 
-    }
-
-    function error(){
-      console.log('paso algooo');
-    }
-    navigator.geolocation.getCurrentPosition(localizacion,error);
+    var appen = `<div class="row">
+    <div class="col s12 m8 push-m2 publications">
+    <h3>${titleDate}</h3>
+    <p>${date}</p>
+    <div id="map" class="map"></div>
+    </div>
+ </div>`
+containerContext.append(appen);
+initMap();
 
   });
 
-  $('#fileButtonVideo').change(function() {
-    var titleVideo = $('#title-video').val();
+  // agregar video
+  $('#fileButtonVideo').change(function () {
     var file = $('#fileButtonVideo').val();
     // console.log(file);
     var allowedExtensions = /(.mp3|.midi|.wav|.WMA)$/i;
-    if(!allowedExtensions.exec(file)){
+    if (!allowedExtensions.exec(file)) {
       // console.log('no es mp3')
       var reader = new FileReader();
-    reader.onload = function(event) {
-      var appen = `<div class="row">
-                   <div class="col s12 m8 push-m2 publications">
-                   <h3>${titleVideo}</h3>
-                   <video src="_pub_" alt="" class="img-pub" controls></video>
-                   </div>
-                </div>`
-      var appenReplace = appen.replace('_pub_', event.target.result);
-      containerContext.append(appenReplace);
-    };
-    reader.readAsDataURL(this.files[0]);
+      reader.onload = function (event) {
+        opcion = 'video';
+        video = event.target.result;
+      };
+      reader.readAsDataURL(this.files[0]);
     } else {
       // console.log('si es mp3')
       var reader = new FileReader();
-    reader.onload = function(event) {
+      reader.onload = function (event) {
+        opcion = 'audio';
+        video = event.target.result;
+      };
+      reader.readAsDataURL(this.files[0]);
+    }
+  });
+
+  $('#public-video').click(function () {
+    var titleVideo = $('#title-video').val();
+    if (opcion === 'video') {
       var appen = `<div class="row">
                    <div class="col s12 m8 push-m2 publications">
                    <h3>${titleVideo}</h3>
-                   <audio src="_pub_" controls></audio>
+                   <video src=${video} alt="" class="img-pub" controls></video>
                    </div>
                 </div>`
-      var appenReplace = appen.replace('_pub_', event.target.result);
-      containerContext.append(appenReplace);
-    };
-    reader.readAsDataURL(this.files[0]);
+      // var appenReplace = appen.replace('_pub_', event.target.result);
+      containerContext.append(appen);
+    } else {
+      var appen = `<div class="row">
+                   <div class="col s12 m8 push-m2 publications">
+                   <h3>${titleVideo}</h3>
+                   <audio src=${video} alt="" controls></audio>
+                   </div>
+                </div>`
+      // var appenReplace = appen.replace('_pub_', event.target.result);
+      containerContext.append(appen);
     }
-  });
+  })
 });
